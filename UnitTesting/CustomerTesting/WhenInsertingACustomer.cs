d0ee8f566af676a8685ffc85ebabc61cee1af041
Paul <paul.j.millar.1971@gmail.com>
2016-11-17 09:58:33 +0000

    Changed Insert Testing Class

--- UnitTesting/CustomerTesting/WhenInsertingACustomer.cs
+++ UnitTesting/CustomerTesting/WhenInsertingACustomer.cs
@@ -23,43 +23,48 @@ namespace UnitTesting.CustomerTesting
         [TestMethod]
         public void Can_Insert_Without_Optional_Address()
         {
+            // Arrange
             var customer = Customer.Create("Mary", GenerateFakeEmail(), "555-666-999", "m@work.com", "444-777-555");
 
+            // Act
             _repo.Insert(customer);
             _repo.SaveChanges();
 
+            // Assert
             Assert.AreNotEqual(0, customer.Id);
         }
 
         [TestMethod]
         public void Can_Insert_With_Optional_Address()
         {
-
+            // Arrange
             var address = Address.Create("SomeStreet", "Some City", "Some County", "Some Country", "Some Postal Code");
             var customer = Customer.Create("Harry", GenerateFakeEmail(), "555-666-999", "harry@work.com", "444-777-555", address);
 
+            // Act
             _repo.Insert(customer);
             _repo.SaveChanges();
 
+            // Assert
             Assert.AreNotEqual(0, customer.Address.Id);
         }
 
-        //[TestMethod]
+        [TestMethod]
         public void Cannot_Insert_With_Existing_Email_Address()
         {
             // Arrange
             var customer = Customer.Create("Harry", "p@gmail.com", "555-666-999", "m@work.com", "444-777-555");
 
-            // Act
+            // Act & Assert
             try
             {
                 _repo.Insert(customer);
                 _repo.SaveChanges();
-                Assert.AreNotEqual(customer.Id, customer.Id);
+                Assert.Fail();
             }
             catch
             {
-                Assert.Fail();
+                Assert.IsTrue(true);
             }
         }
 
